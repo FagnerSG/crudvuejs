cebd5e3996192ffe576b25c13bfb765d2faf9b2a
FagnerSG <fagnersgx@gmail.com>
2023-11-12 13:34:22 -0300

    adicionar script de salvar arquivo

--- src/App.vue
+++ src/App.vue
@@ -6,64 +6,82 @@
       </div>
     </v-app-bar>
 
-  <v-container>  
-    <v-row class="text-center">
-      <v-spacer>
-      <v-col offset-lg="3" lg="6" md="12">
-        <v-rol>
-          <v-col cols="10">
-            <v-text-field label="Filme" variant="outlined"></v-text-field>
-          </v-col>
-          <v-col cols="12" class="text-right">
-            <v-btn class="bg-blue">Adicionar</v-btn>
-          </v-col>
-        </v-rol>
-        <v-row>
+  <v-container>   
+    <v-row >
+      <v-rol>
+        <v-col cols="12" md="6">
+          <v-text-field v-model="filmeNome" label="Filme" variant="outlined"></v-text-field>
+        </v-col>
+      </v-rol>  
+      <v-rol>
+        <v-col cols="12" md="4" class="text-right">
+          <v-btn @click="adicionaFilme" class="bg-blue">Adicionar</v-btn>
+        </v-col>
+      </v-rol>
+    </v-row>
+    
 
-        </v-row>
-      </v-col>
-    </v-spacer>
+
+    <v-row >
+      <v-rol>
+        <v-col offset-lg="2" lg="8" md="10">
+          <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">Filmes</th>
+              </tr>
+            </thead>
+          </v-table>      
+        </v-col>
+      </v-rol>
     </v-row>
 
-    <row class="text-center">
-      <v-col offset-lg="2" lg="8" md="12">
-        <v-table>
-          <thead>
-            <tr>
-              <th class="text-left" style="width: 60%">Filme</th>
-              <th class="text-right"> </th>
-            </tr>
-          </thead>
-        </v-table>
-      </v-col>
-    </row>
 
-    <tbody>
-      <v-field class="text-center">
-      <tr>
-        <td class="text-left" style="width: 85%">
-          NomeFilme
-        </td>
-        <td class="text-center" style="width:50%;">
+   <v-table class="bg-green">     
+      <v-field>
+        <v-row >
+          <v-col cols="12" md="6">
+            <v-textarea v-model="entradafilme" label="NomeFilme" variant="outlined"></v-textarea>
+            <td class="text-left" style="width: 80%">
+              NomeFilme
+            </td>
+          
+          </v-col>
+        </v-row>
+        <v-col>
+          <td class="text-left">
           <v-select 
-            label="Select" 
-              :items="['aguardando', 'assistindo', 'finalizado']" 
-              variant="outlined">
+            v-model="estilofilme"  
+                :items="['Acao', 'Animacao', 'Ficcao']"
+                label="Estilo"
+                multiple
+                variant="outlined">
           </v-select>
         </td>
-      </tr>
-    </v-field>
-    </tbody>
- 
-    <v-select 
-      v-model="estilofilme"  
-          :items="['Acao', 'Animacao', 'Ficcao']"
-          label="Estilo"
-          multiple
-          variant="outlined">
-    </v-select>
+        </v-col>
+      </v-field>
 
-    
+      <v-field>
+        <v-row>
+          <v-rol>
+            <v-col>
+              <v-select 
+                label="Select"
+                  :value="selecao" 
+                  :items="['aguardando', 'assistindo', 'finalizado']" 
+                  variant="outlined">
+              </v-select>
+            </v-col>
+            <v-col>
+              <v-btn @click="excluir" class="bg-red">Excluir</v-btn>
+            </v-col>
+          
+          </v-rol>
+        </v-row>
+      </v-field>
+    </v-table>
+  
+
   </v-container>
     
     <v-main>
@@ -75,6 +93,10 @@
 
 <script>
 import FormularioFilme from './components/FormularioFilme.vue'
+import App from './App.vue'
+import { createApp } from 'vue';
+
+createApp(App) 
 export default {
   name: 'App',
 
@@ -84,11 +106,20 @@ export default {
   },
 
   data: () => ({
-    //items: ['Acao', 'Animacao', 'Ficcao'],
-    estilofilme: ['Acao', 'Animacao', 'Ficcao'],
-    //
+    filmeNome: '',
+    entradafilme: '',
+    estilofilme: ['Acao', 'Animacao', 'Ficcao'], 
+
   }),
+
+  methods: {
+     adicionaFilme(){
+      this.entradafilme += this.filmeNome + '';
+      this.filmeNome ='';
+    },
+  }
 }
+
 </script>
 
 <style>
